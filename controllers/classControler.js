b633dad4d7f63913f5eae8852ca454b5fc26d5e1
Sikora <carlen.sikora@accenture.com>
2022-11-07 17:09:42 +1100

    Attempting to implement csv file reading functionality for relational data

--- controllers/classControler.js
+++ controllers/classControler.js
@@ -1,14 +1,20 @@
 //Handle requests to get data instance from API
 //import demo
 const classData = require('../schema/timetable')
-//const csv = require('csvtojson')
 
+//These 3 lines are not working
+/*const fs = require('fs')
+const csv = require('csvtojson')
+const csvdata = require('../controllers/relational.csv')*/
 
-const getAllClasses = async(req,res) =>{
-    // console.log(classData)
-
-    //const data = await csv.fromFile('../schema/relationalData.csv');
-    //console.log(data);
+const getAllClasses = (req,res) =>{
+    //The below 3 lines also are not working... 
+    //thought process, await and async need to 
+    //be involved however not implementing properly
+    
+    /*console.log(csvdata)
+    const data =  csv().fromFile(csvdata);
+    console.log(data[0]);*/
     return res.render("classesPage.hbs",{data: classData})
 
 }
