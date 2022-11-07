166ce70849b4c70032d4b91fc9f8e021c92ab710
Sikora <carlen.sikora@accenture.com>
2022-11-07 15:44:20 +1100

    attempting to implement csv relational data

--- controllers/classControler.js
+++ controllers/classControler.js
@@ -1,9 +1,14 @@
 //Handle requests to get data instance from API
 //import demo
 const classData = require('../schema/timetable')
+//const csv = require('csvtojson')
 
-const getAllClasses = (req,res) =>{
+
+const getAllClasses = async(req,res) =>{
     // console.log(classData)
+
+    //const data = await csv.fromFile('../schema/relationalData.csv');
+    //console.log(data);
     return res.render("classesPage.hbs",{data: classData})
 
 }
